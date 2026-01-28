43114517ddacf74d357c193c20304187b6f6084b
최인수 <insu@choeinsuui-noteubug.local>
2026-01-28 16:31:43 +0900

    insu

--- script/script.js
+++ script/script.js
@@ -1,7 +1,26 @@
 
 
 document.addEventListener("DOMContentLoaded", function(e){
-
+    canvas();
 })
 
 
+const canvas =() => {
+    
+    var granimInstance = new Granim({
+        element: '#canvas-basic',
+        direction: 'left-right',
+        isPausedWhenNotInView: true,
+        transitionSpeed: 2000,
+        loopDuration: 1000,
+        states : {
+            "default-state": {
+                gradients: [
+                    ['#1E242F', '#3B4C60'],  // 네이비 블루 계열로 확실한 대비
+                    ['#000000', '#264D7D'],  // 밝은 블루그레이, 부드럽게 이어짐
+                    ['#1E242F', '#000000']   // 훨씬 어두운 컬러로 깊이감 추가
+                ]
+            }
+        }
+    });
+}
